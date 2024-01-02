18f477229e63f8237e509c0678222881c2822f7c
Anh Lam Truong <85408593+anhlamtruong@users.noreply.github.com>
2024-01-01 23:16:42 -0500

    tesy

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import prismaAuthenticate from "@/lib/db/authenticate_db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getUserById } from "@/data/user";
+import { UserRole } from "@prisma-client-authenticate";
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +11,29 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ sessionToken: token, session });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prismaAuthenticate),
   session: { strategy: "jwt" },
   ...authConfig,
---
+++ next-auth.d.ts
@@ -0,0 +1,24 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+import { UserRole } from "@prisma-client-authenticate";
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+};
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: ExtendedUser; // To keep the default types
+  }
+}
+
+// import { JWT } from "next-auth/jwt";
+
+// declare module "next-auth/jwt" {
+//   /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+//   interface JWT {
+//     /** OpenID ID Token */
+//     role?: UserRole;
+//   }
+// }
