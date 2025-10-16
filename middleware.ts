01acc0918e1eacc3bebea5a68a9fe1ba26104e10
Lam Anh Truong <emcaemchut@Lams-MacBook-Pro.local>
2025-10-16 14:30:49 -0400

    hotfix

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
-import authConfig from "@/auth.config";
-import NextAuth from "next-auth";
-const { auth } = NextAuth(authConfig);
+import { auth } from "@/auth";
 import {
   DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
