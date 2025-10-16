d2e60b034f5d1e6b67184f460779a00a28366dfb
Lam Anh Truong <emcaemchut@Lams-MacBook-Pro.local>
2025-10-15 22:06:52 -0400

    fix size middleware

--- middleware.ts
+++ middleware.ts
@@ -13,22 +13,6 @@ import {
   authCLIRoutes,
 } from "@/routes";
 
-const corsOptions: {
-  allowedMethods: string[];
-  allowedOrigins: string[];
-  allowedHeaders: string[];
-  exposedHeaders: string[];
-  maxAge?: number;
-  credentials: boolean;
-} = {
-  allowedMethods: (process.env?.ALLOWED_METHODS || "").split(","),
-  allowedOrigins: (process.env?.ALLOWED_ORIGIN || "").split(","),
-  allowedHeaders: (process.env?.ALLOWED_HEADERS || "").split(","),
-  exposedHeaders: (process.env?.EXPOSED_HEADERS || "").split(","),
-  maxAge: (process.env?.MAX_AGE && parseInt(process.env?.MAX_AGE)) || undefined, // 60 * 60 * 24 * 30, // 30 days
-  credentials: process.env?.CREDENTIALS == "true",
-};
-
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
@@ -53,36 +37,6 @@ export default auth((req) => {
     const response = NextResponse.next();
     // Allowed origins check
     const origin = req.headers.get("origin") ?? "";
-    if (
-      corsOptions.allowedOrigins.includes("*") ||
-      corsOptions.allowedOrigins.includes(origin)
-    ) {
-      response.headers.set("Access-Control-Allow-Origin", origin);
-    }
-    // Set default CORS headers
-    response.headers.set(
-      "Access-Control-Allow-Credentials",
-      corsOptions.credentials.toString()
-    );
-    response.headers.set(
-      "Access-Control-Allow-Methods",
-      corsOptions.allowedMethods.join(",")
-    );
-    response.headers.set(
-      "Access-Control-Allow-Headers",
-      corsOptions.allowedHeaders.join(",")
-    );
-    response.headers.set(
-      "Access-Control-Expose-Headers",
-      corsOptions.exposedHeaders.join(",")
-    );
-    response.headers.set(
-      "Access-Control-Max-Age",
-      corsOptions.maxAge?.toString() ?? ""
-    );
-
-    // Return
-    return response;
   }
   if (isAuthRoute) {
     if (isLoggedIn) {
